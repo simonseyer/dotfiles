1f8c507c6a22b35010edf5d4901392ef40d2b08c
Simon Seyer <simon@comeup.ch>
2026-02-28 11:43:50 +0100

    Hide dock automatically

--- scripts/apply_defaults.sh
+++ scripts/apply_defaults.sh
@@ -1,3 +1,4 @@
 defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled 0
 defaults write com.apple.dock no-bouncing -bool TRUE
+defaults write com.apple.dock autohide -bool true
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
